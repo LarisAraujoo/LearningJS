424cab34cdc594c21a14753e20aaceab5c50325b
Larissa Araujo <lari_araujo093@hotmail.com>
2024-06-21 19:44:54 -0300

    attuzalicao

---
+++ Aula010/app.js
@@ -0,0 +1,9 @@
+let weekend = prompt('What date is today?')
+
+if (weekend === 'Saturday') {
+    alert('Have a nice weekend!')
+} else if (weekend === 'Sunday') {
+    alert('Have a nice weekend!')
+} else {
+    alert('Have a nice week!')
+}
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,10 +2,13 @@ alert('Welcome to the first game Secret Number!')
 
 let number = Number(prompt('Digite um numero de 1 à 10:'))
 
+
 let secretNumber = 5
 
+console.log(secretNumber)
+
 if (number == secretNumber) {
-    console.log('You win!')
+    alert(`The number is ${secretNumber}. You win!`)
 } else {
-    console.log('You lose, try again!')
+    alert('You lose, try again!')
 }
