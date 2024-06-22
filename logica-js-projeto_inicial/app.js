dc1115ad3ce27c9514b2bd80da5cdafb40551de2
Larissa Araujo <lari_araujo093@hotmail.com>
2024-06-22 13:54:50 -0300

    att

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,14 +1,19 @@
 alert('Welcome to the first game Secret Number!')
+let secretNumber = 6
+let number 
+let attempts = 1
 
-let number = Number(prompt('Digite um numero de 1 à 10:'))
+while (number != secretNumber){
+    number = Number(prompt('Enter a number from 0 to 10:'))
 
-
-let secretNumber = 5
-
-console.log(secretNumber)
-
-if (number == secretNumber) {
-    alert(`The number is ${secretNumber}. You win!`)
-} else {
-    alert('You lose, try again!')
+    if (number == secretNumber){
+        alert(`The number is ${secretNumber}, you win with ${attempts} attempts!`)
+    } else {
+        if (number > secretNumber){
+            alert(`The number is less than ${number}`)
+        } else {
+            alert(`The number is greater than ${number}`)
+        }
+        attempts++
+    }
 }
