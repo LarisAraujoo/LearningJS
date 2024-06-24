52e2ffaf5edca793abdf270529ec53129af7a578
Larissa Araujo <lari_araujo093@hotmail.com>
2024-06-24 20:09:40 -0300

    att

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Adivinhe o número:';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero de 0 à 10';
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+}
